ffdad113359f8a2072daec7929ab18cba50a054a
Aloula <aloula.afewerki@gmail.com>
2025-03-28 16:40:46 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,75 @@
+
+// Pour la partie de Darkmode et light mode
+const toggleButton = document.getElementById('theme-toggle');
+const body = document.body;
+
+body.classList.add('light-mode');
+
+toggleButton.addEventListener('click', () => {
+    if (body.classList.contains('light-mode')) {
+        body.classList.replace('light-mode', 'dark-mode');
+        toggleButton.textContent = 'Light Mode';
+        toggleButton.style.backgroundColor = "black";
+    } else {
+        body.classList.replace('dark-mode', 'light-mode');
+        toggleButton.textContent = 'Dark Mode';
+       
+    }
+});
+
+// Ajout paragraphe de la partie de service 
+const service = document.getElementById('service');
+let exist = false;
+
+service.addEventListener('click', () => {
+  if (exist) {
+    const liste = document.querySelector('#liste');
+    liste.remove();
+    exist = false;
+  } else {
+    const liste = document.createElement('ul'); 
+    liste.id = 'liste';
+
+    const services = [
+      "Photographie de la nature",
+      "Photographie d'événements",
+      "Portraits",
+      "Retouche photo"
+    ];
+
+    services.forEach(service => {
+      const item = document.createElement('li'); 
+      item.textContent = service;
+      liste.appendChild(item);
+    });
+
+    service.appendChild(liste); 
+    exist = true;
+  }
+});
+    
+  
+
+//     //light box
+//     const galleryImages = document.querySelectorAll('.gallery-img');
+// const lightbox = document.getElementById('lightbox');
+// const lightboxImg = document.getElementById('lightbox-img');
+// const closeLightbox = document.getElementById('close-lightbox');
+
+// galleryImages.forEach(img => {
+//   img.addEventListener('click', () => {
+//     lightboxImg.src = img.src;
+//     lightbox.style.display = 'block';
+//   });
+// });
+
+// closeLightbox.addEventListener('click', () => {
+//   lightbox.style.display = 'none';
+// });
+
+// // Close lightbox when clicking outside the image
+// lightbox.addEventListener('click', (e) => {
+//   if (e.target === lightbox) {
+//     lightbox.style.display = 'none';
+//   }
+// });
